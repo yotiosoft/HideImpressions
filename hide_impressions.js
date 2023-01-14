59c96c35b3fd6d9b588a66f92a72c26c06598ea5
YotioSoft <ytani0323@gmail.com>
2023-01-15 02:11:54 +0900

    release ver.1.1 for fix

--- hide_impressions.js
+++ hide_impressions.js
@@ -1,22 +1,27 @@
 const observer = new MutationObserver(onRun);
 observer.observe(document.body, {childList: true, subtree: true});
+
+function hide_parent_if_analytics_link(class_name) {
+  var elements = document.body.getElementsByClassName(class_name);
+
+  for (element of elements) {
+    var href_link = element.getAttribute('href'); 
+    if (href_link != null && href_link.match(/analytics/)) {
+      element.parentNode.style.display = "none";
+    }
+  }
+}
+
+function hide_element(class_name) {
+  var elements = document.body.getElementsByClassName(class_name);
+
+  for (element of elements) {
+    element.style.display = "none";
+  }
+}
   
 function onRun() {
-  var link_elements = document.body.getElementsByClassName("css-4rbku5 css-18t94o4 css-1dbjc4n r-1loqt21 r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr");
-
-  for (element of link_elements) {
-    var href_link = element.getAttribute('href'); 
-    if (href_link != null && href_link.match(/analytics/)) {
-      element.parentNode.style.display = "none";
-    }
-  }
-
-  var link_elements2 = document.body.getElementsByClassName("css-4rbku5 css-18t94o4 css-901oao r-18jsvk2 r-1loqt21 r-1tl8opc r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0");
-
-  for (element of link_elements2) {
-    var href_link = element.getAttribute('href'); 
-    if (href_link != null && href_link.match(/analytics/)) {
-      element.parentNode.style.display = "none";
-    }
-  }
+  hide_parent_if_analytics_link("css-4rbku5 css-18t94o4 css-1dbjc4n r-1loqt21 r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr");
+  hide_element("css-901oao css-16my406 r-18jsvk2 r-1tl8opc r-bcqeeo r-qvutc0");
+  hide_element("css-901oao css-16my406 r-14j79pv r-1q142lx r-1tl8opc r-bcqeeo r-s1qlax r-qvutc0");
 }
