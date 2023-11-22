8b68b57ab8cef2bcc3a4e75e7f2f850b989652a6
YotioSoft <ytani0323@gmail.com>
2023-11-22 16:55:13 +0900

    fix class names

--- hide_impressions.js
+++ hide_impressions.js
@@ -32,8 +32,9 @@ function hide_element_if_parent_and_child(parent_class_name, class_name) {
 }
 
 function onRun() {
-  hide_parent_if_analytics_link("css-4rbku5 css-18t94o4 css-1dbjc4n r-1loqt21 r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr");
+  hide_parent_if_analytics_link("css-175oi2r r-1777fci r-bt1l66 r-bztko3 r-lrvibr r-1ny4l3l r-1loqt21");
   //hide_element("css-901oao css-16my406 r-18jsvk2 r-1tl8opc r-bcqeeo r-qvutc0");
-  hide_element_if_parent_and_child("css-1dbjc4n r-1d09ksm r-1471scf r-18u37iz r-1wbh5a2", "css-901oao r-18jsvk2 r-1tl8opc r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0");
-  hide_element_if_parent_and_child("css-1dbjc4n r-1d09ksm r-1471scf r-18u37iz r-1wbh5a2", "css-901oao r-14j79pv r-1q142lx r-1tl8opc r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-s1qlax r-qvutc0");
+  hide_element_if_parent_and_child("css-175oi2r r-1d09ksm r-18u37iz r-1wbh5a2 r-1471scf", "css-1rynq56 r-bcqeeo r-qvutc0 r-1tl8opc r-a023e6 r-rjixqe r-16dba41 r-1q142lx r-s1qlax");
+  hide_element_if_parent_and_child("css-1qaijid r-bcqeeo r-qvutc0 r-1tl8opc", "css-175oi2r r-xoduu5 r-1udh08x");
+  hide_element_if_parent_and_child("css-1qaijid r-bcqeeo r-qvutc0 r-1tl8opc", "css-1qaijid r-bcqeeo r-qvutc0 r-1tl8opc r-1b43r93 r-1cwl3u0");
 }
