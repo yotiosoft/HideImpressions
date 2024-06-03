4d1abe8200c52d5e2aa1e81b5ab91b91c3f8fa4a
yotiosoft <yotio@yotiosoft.com>
2024-06-03 17:54:53 +0900

    ]ver 1.5.1

--- hide_impressions.js
+++ hide_impressions.js
@@ -31,6 +31,20 @@ function hide_element_if_parent_and_child(parent_class_name, class_name) {
   }
 }
 
+function hide_element_if_grandparent_and_parent_and_child(grandparent_class_name, parent_class_name, class_name) {
+  var elements = document.body.getElementsByClassName(class_name);
+
+  for (var element of elements) {
+    var parent = element.parentNode;
+    if (parent.className == parent_class_name) {
+      var grandparent = parent.parentNode;
+      if (grandparent.className == grandparent_class_name) {
+        element.style.display = "none";
+      }
+    }
+  }
+}
+
 function onRun() {
   hide_parent_if_analytics_link("css-175oi2r r-1777fci r-bt1l66 r-bztko3 r-lrvibr r-1ny4l3l r-1loqt21");
   //hide_element("css-901oao css-16my406 r-18jsvk2 r-1tl8opc r-bcqeeo r-qvutc0");
@@ -39,7 +53,9 @@ function onRun() {
   hide_element_if_parent_and_child("css-1qaijid r-bcqeeo r-qvutc0 r-1tl8opc", "css-1qaijid r-bcqeeo r-qvutc0 r-1tl8opc r-1b43r93 r-1cwl3u0");
   
   // ver.1.5
+  hide_element_if_grandparent_and_parent_and_child("css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc r-a023e6 r-rjixqe r-16dba41", "css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc", "css-175oi2r r-xoduu5 r-1udh08x");
+  hide_element_if_grandparent_and_parent_and_child("css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc r-a023e6 r-rjixqe r-16dba41", "css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc", "css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc r-1b43r93 r-1cwl3u0");
+  
   hide_element_if_parent_and_child("css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc r-a023e6 r-rjixqe r-16dba41 r-1q142lx r-n7gxbd", "css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc");
-  hide_element_if_parent_and_child("css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc", "css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc");
-  hide_element_if_parent_and_child("css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc", "css-175oi2r r-xoduu5 r-1udh08x");
+  hide_element_if_parent_and_child("css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-1tl8opc r-a023e6 r-rjixqe r-16dba41 r-1q142lx r-n7gxbd", "css-175oi2r r-xoduu5 r-1udh08x");
 }
